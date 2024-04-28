fa6a9b946d66142476633d4b0251c111b5927d1c
Sneha Jaikumar <snehaj@email.unc.edu>
2024-04-28 16:01:03 -0400

    Add 4 CSV files for pseudobulk

--- comp_683.py
+++ comp_683.py
@@ -44,6 +44,8 @@ def pseudobulk_feats(data = None, meta_label = 'meta_label', end_range = None):
     pseudobulk_feats.columns = ['{}_exp_{}'.format(i, j) for i, j in pseudobulk_feats.columns]
     return pseudobulk_feats
 
+
+
 # 35450 × 3000
 adata_merge = sc.read_h5ad('/Users/snehajaikumar/CompScience/comp683/merged_adata.h5ad')
 sc.pp.highly_variable_genes(adata_merge, flavor = 'seurat', n_top_genes = 2000, subset = True)
@@ -52,65 +54,86 @@ data_mod['sample_id'] = adata_merge.obs['shortname'].copy()
 data_mod['cluster_label'] = adata_merge.obs['leiden'].copy()
 data_mod['age'] = adata_merge.obs['age'].copy()
 
+
+def convert_gest_age_to_days(gest_age_str):
+    weeks, days = map(int, gest_age_str.split('+'))
+    return weeks * 7 + days
+
+
 #pseudobulk preprocessing
 result_1 = pseudobulk_feats(data = data_mod, meta_label = 'cluster_label', end_range = 2000)
 column_labels = list(result_1.columns)
 unique_genes_ordered = list(OrderedDict.fromkeys(element.split("_")[0] for element in column_labels))
 result = result_1.astype(float)
 result_temp = result.apply(lambda x: np.log(x+1), axis=1)
-reshaped_data = result_temp.squeeze()
-reshaped_data = reshaped_data.values.reshape(48, -1, 21)
-averages = np.mean(reshaped_data, axis=2)
-averages_df = pd.DataFrame(averages, index=result.index, columns=range(0, 2000))
-averages_df.columns = unique_genes_ordered
-filtered_pseudo_temp = averages_df#[common_elements]
+# reshaped_data = result_temp.squeeze()
+# reshaped_data = reshaped_data.values.reshape(48, -1, 21)
+# averages = np.mean(reshaped_data, axis=2)
+# averages_df = pd.DataFrame(averages, index=result.index, columns=range(0, 2000))
+# averages_df.columns = unique_genes_ordered
+# filtered_pseudo_temp = averages_df#[common_elements]
 #Standard scaler
 scaler = StandardScaler()
-scaler.fit(filtered_pseudo_temp)
-scaled_data = scaler.transform(filtered_pseudo_temp)
-filtered_pseudo = pd.DataFrame(scaled_data, columns = filtered_pseudo_temp.columns)
+scaler.fit(result_temp)
+scaled_data = scaler.transform(result_temp)
+filtered_pseudo = pd.DataFrame(scaled_data, columns = result_temp.columns)
 #Add age column back to df now
+filtered_pseudo["sample_name"] = result_1.index.str.split('_').str[0]
 filtered_pseudo["Age"] = result_1.index.str.split('_').str[1]
-#Drop age column
-pseudo_bulk_1 =  filtered_pseudo.drop(["Age"], axis=1)
-#Add age column back again
-pseudo_bulk_1.insert(0,"Age",filtered_pseudo.Age)
+print(filtered_pseudo)
+
+# #Drop age column
+# pseudo_bulk_1 =  filtered_pseudo.drop(["Age"], axis=1)
+# #Add age column back again
+# pseudo_bulk_1.insert(0,"Age",filtered_pseudo.Age)
 
 #View Hammond pseudobulk
-print(pseudo_bulk_1)
+# print(pseudo_bulk_1)
+filtered_pseudo.to_csv("hammond_pseudobulk.csv")
 
 #_________________________________________________
 # 17295 × 677
-adata_merge = sc.read_h5ad('/Users/snehajaikumar/CompScience/comp683/adata_merge_fetuses_leiden.h5ad')
-data_mod = pd.DataFrame(adata_merge.X, columns = adata_merge.var_names, index = adata_merge.obs_names)
-data_mod['sample_id'] = adata_merge.obs['shortname'].copy()
-data_mod['cluster_label'] = adata_merge.obs['leiden'].copy()
-data_mod['age'] = adata_merge.obs['age'].copy()
+# adata_merge = sc.read_h5ad('/Users/snehajaikumar/CompScience/comp683/adata_merge_fetuses_leiden.h5ad')
+# data_mod = pd.DataFrame(adata_merge.X, columns = adata_merge.var_names, index = adata_merge.obs_names)
+# data_mod['sample_id'] = adata_merge.obs['shortname'].copy()
+# data_mod['cluster_label'] = adata_merge.obs['leiden'].copy()
+# data_mod['age'] = adata_merge.obs['age'].copy()
+# print("initial age",data_mod['age'])
 
-result_1 = pseudobulk_feats(data = data_mod, meta_label = 'cluster_label', end_range = 677)
-column_labels = list(result_1.columns)
-unique_genes_ordered = list(OrderedDict.fromkeys(element.split("_")[0] for element in column_labels))
-result = result_1.astype(float)
-result_temp = result.apply(lambda x: np.log(x+1), axis=1)
-# 239 x 10800, 16 clusters
-#Take the average of genes across all 16 clusters
-reshaped_data = result_temp.squeeze()
-reshaped_data = reshaped_data.values.reshape(239, -1, 16)
-averages = np.mean(reshaped_data, axis=2)
-averages_df = pd.DataFrame(averages, index=result.index, columns=range(0, 675))
-averages_df.columns = unique_genes_ordered
-filtered_pseudo_temp = averages_df#[common_elements]
-# Standard scaler
-scaler = StandardScaler()
-scaler.fit(filtered_pseudo_temp)
-scaled_data = scaler.transform(filtered_pseudo_temp)
-filtered_pseudo = pd.DataFrame(scaled_data, columns = filtered_pseudo_temp.columns)
-# Add age column back to df now
-filtered_pseudo["Age"] = result_1.index.str.split('_').str[1]
-# Drop age column
-pseudo_bulk_1 =  filtered_pseudo.drop(["Age"], axis=1)
-# Add age column back again
-pseudo_bulk_1.insert(0,"Age",filtered_pseudo.Age)
+# def convert_gest_age_to_days(gest_age_str):
+#     weeks, days = map(int, gest_age_str.split('+'))
+#     return weeks * 7 + days
 
-#View Fetus Pseudobulk
-print(pseudo_bulk_1)
+
+# result_1 = pseudobulk_feats(data = data_mod, meta_label = 'cluster_label', end_range = 677)
+# column_labels = list(result_1.columns)
+# unique_genes_ordered = list(OrderedDict.fromkeys(element.split("_")[0] for element in column_labels))
+# result = result_1.astype(float)
+# result_temp = result.apply(lambda x: np.log(x+1), axis=1)
+# print(result_temp)
+# # 239 x 10800, 16 clusters
+# #Take the average of genes across all 16 clusters
+# # reshaped_data = result_temp.squeeze()
+# # reshaped_data = reshaped_data.values.reshape(239, -1, 16)
+# # averages = np.mean(reshaped_data, axis=2)
+# # averages_df = pd.DataFrame(averages, index=result.index, columns=range(0, 675))
+# # averages_df.columns = unique_genes_ordered
+# # filtered_pseudo_temp = averages_df#[common_elements]
+# # Standard scaler
+# scaler = StandardScaler()
+# scaler.fit(result_temp)
+# scaled_data = scaler.transform(result_temp)
+# filtered_pseudo = pd.DataFrame(scaled_data, columns = result_temp.columns)
+# print(filtered_pseudo.head())
+# # Add age column back to df now
+# df_metadata = pd.read_excel('fetuses_metadata.xlsx', usecols=['sample_name', 'Gestational week','Gestational age (weeks + days)'])
+# filtered_pseudo['sample_name'] = result_temp.index.str.split('_').str[0]
+# merged_df = pd.merge(filtered_pseudo, df_metadata, on='sample_name', how='left')
+# merged_df['Gestational age (days)'] = merged_df['Gestational age (weeks + days)'].apply(convert_gest_age_to_days)
+# print(merged_df)
+# # pseudo_bulk_1 =  merged_df.drop(["Gestational age (days)"], axis=1)
+# # pseudo_bulk_1.insert(0,"Age(days)",merged_df."Gestational age(days)")
+
+# # #View Fetus Pseudobulk
+# # print(pseudo_bulk_1)
+# merged_df.to_csv("fetus_pseudobulk.csv")
